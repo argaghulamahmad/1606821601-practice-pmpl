afa80c3bd62f4904ec3004ce8c5068f0e3baf79b
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2019-09-15 14:04:03 +0700

    Set project directory and static root

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+# Set project directory and static root
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
